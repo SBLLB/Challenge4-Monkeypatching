57e55e311691f833fc8e7ba3fee53883572b39c1
RachelNolan <mail@rachelnolan.com>
2014-09-27 13:54:16 +0100

    Replacement inject method now takes block and enumerator passed

--- lib/array.rb
+++ lib/array.rb
@@ -11,12 +11,18 @@ class Array
 
 	def recursive_inject
 		return 0 unless self.length > 0
-		return self.shift + self.recursive_inject
+		self.shift + self.recursive_inject
+	end
 
+	def total_inject(accumulator)
+		self[0..-1].each do |item|
+			accumulator = yield(accumulator, item)
+		end
+		return accumulator
 	end
 
 end
 
-p [1, 2, 3, 4].iterator_inject
+# p [1, 2, 3, 4].iterator_inject
 
-p [1, 2, 3, 4].recursive_inject
+# p [1, 2, 3, 4].recursive_inject
--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -1,15 +1,28 @@
 require 'array'
 
 describe Array do 
+	context 'The replacement inject methods should' do 
+		context 'should return the sum of numbers in an array ' do 
 
-	 it 'should return the sum of numbers in an array using an iterator' do 
-	 	expect([1, 2, 3, 4].iterator_inject).to eq(10)
-	 end
- 	
- 	it 'should return the sum of numbers in an array using recursion' do 
-	 	expect([1, 2, 3, 4].recursive_inject).to eq(10)
+		arr = [1, 2, 3, 4]
+			 it 'using an iterator' do 
+			 	expect(arr.iterator_inject() ).to eq(10)
+			 end
+		 	
+		 	it 'using recursion' do 
+			 	expect(arr.recursive_inject).to eq 10 
+			end
+		end
+
+		context 'should return the results of a block passed' do 
+			
+			it 'with an enumerator passed as an argument' do 
+				expect([1, 2, 3, 4].total_inject(0) { |accumulator, item| accumulator + item }).to eq(10)
+			end
+		end 
 	end
 end
 
 
-#[1, 2, 3, 4].inject { |result, element| result + element }
+# [1, 2, 3, 4].inject() { |result, element| result * element }
+
